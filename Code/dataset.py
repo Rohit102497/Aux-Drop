a4233fd5dc06b58fcb99b2c66424d27ce16a2fb3
Rohit Agarwal <agarwal.102497@gmail.com>
2023-04-20 01:39:48 +0200

    support for a8a dataset is added

--- Code/dataset.py
+++ Code/dataset.py
@@ -24,13 +24,13 @@ import os
 def dataset(name = "german"):
     # Values to change
     n = 0.1
-    aux_feat_prob = 0.27
+    aux_feat_prob = 0.25
     dropout_p = 0.3
     max_num_hidden_layers = 6
     qtd_neuron_per_hidden_layer = 50
     n_classes = 2
     aux_layer = 3
-    n_neuron_aux_layer = 100
+    n_neuron_aux_layer = 400
     batch_size = 1
     b = 0.99
     s = 0.2
@@ -137,7 +137,40 @@ def dataset(name = "german"):
         return n_base_feat, max_num_hidden_layers, qtd_neuron_per_hidden_layer, \
                 n_classes, aux_layer, n_neuron_aux_layer, batch_size, b,  n, s, \
                 dropout_p, n_aux_feat,  use_cuda, X_base, X_aux_new, aux_mask, Y, label
-        
+
+    if name == "a8a":
+        # Data description
+        # Path to data
+        data_path = os.path.join(os.path.dirname(__file__), 'Datasets', name, 'a8a.txt')
+        n_feat = 123
+        n_aux_feat = 121
+        number_of_instances = 32561
+
+        data = pd.DataFrame(0, index=range(number_of_instances), columns = list(range(1, n_feat+1)))
+        # reading csv files
+        data_initial =  pd.read_csv(data_path, sep=" ", header=None)
+        data_initial = data_initial.iloc[:, :15]
+        for j in range(data_initial.shape[0]):
+                l = [int(i.split(":")[0])-1 for i in list(data_initial.iloc[j, 1:]) if not pd.isnull(i)]
+                data.iloc[j, l] = 1
+        label = np.array(data_initial[0] == -1)*1
+        data.insert(0, column='class', value=label)
+        data = data.sample(frac = 1)
+        label = np.array(data["class"])
+
+        # Masking
+        aux_mask = (np.random.random((number_of_instances, n_aux_feat)) < aux_feat_prob).astype(float)
+
+        # Data division
+        n_base_feat = data.shape[1] - 1 - n_aux_feat
+        Y = np.array(data.iloc[:,:1])
+        X_base = np.array(data.iloc[:,1:n_base_feat+1], dtype = float)
+        X_aux = np.array(data.iloc[:,n_base_feat+1:], dtype = float)
+        X_aux_new = np.where(aux_mask, X_aux, 0)
+
+        return n_base_feat, max_num_hidden_layers, qtd_neuron_per_hidden_layer, \
+                n_classes, aux_layer, n_neuron_aux_layer, batch_size, b,  n, s, \
+                dropout_p, n_aux_feat,  use_cuda, X_base, X_aux_new, aux_mask, Y, label
 
     else:
         print("The data name entered is wrong. Please type one of the following names: german, svmguide3, magic04, a8a")
